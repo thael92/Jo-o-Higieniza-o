6616e75c2caba469a9f1a8174ed3734322b23565
thael92 <thael92@gmail.com>
2024-11-12 15:30:24 -0300

    Projeto J.S Clean

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+
+function cliqueinobotao() {
+    form.style.left = "50%"
+    form.style.transform = "translatex(-50%)"
+    mascara.style.visibility = "visible"
+}
+function esconderfor(){
+form.style.left="-456px"
+form.style.transform="translatex(50%)"
+mascara.style.visibility = "hidden"
+}
+function faleconosco(){
+    form.style.left = "50%"
+    form.style.transform = "translatex(-50%)"
+    mascara.style.visibility = "visible"
+}
+function faleconosco2(){
+    form.style.left = "50%"
+    form.style.transform = "translatex(-50%)"
+    mascara.style.visibility = "visible"
+}
